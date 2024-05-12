4b963f5070a5c3d24f4daffe7b1037508a89b776
dolichotis <60782357+dolichotis@users.noreply.github.com>
2024-05-12 23:09:58 +0300

    internet connection

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Row, Col, Alert } from 'antd';
+import { Row, Col, Alert, Spin } from 'antd';
 import 'antd/dist/reset.css';
+import { Offline, Online } from 'react-detect-offline';
 import './app.css';
 
 import SwapiService from '../../services/swapi-service';
 import FilmPoster from '../film-poster/film-poster';
-import Loader from '../loader/loader';
 
 export default class App extends Component {
   constructor(props) {
@@ -14,12 +14,18 @@ export default class App extends Component {
       films: [],
       error: false,
       loading: true,
+      isOnline: navigator.onLine,
     };
     this.swapiService = new SwapiService();
   }
 
   componentDidMount() {
     setTimeout(() => {
+      if (!this.state.isOnline) {
+        this.setState({ error: 'Нет подключения к интернету', loading: false });
+        return;
+      }
+
       this.swapiService
         .getMovies()
         .then((data) => {
@@ -42,20 +48,23 @@ export default class App extends Component {
     const { films, error, loading } = this.state;
 
     const isError = error ? <Alert message={error} description="Возникла ошибка!" type="error" showIcon /> : null;
-
-    if (loading) {
-      return <Loader />;
-    }
+    const spin = loading && !isError ? <Spin tip="Loading..." size="large" fullscreen /> : null;
 
     return (
       <section className="main">
         <Row>
-          {films.map((film) => (
-            <Col key={film.id} xs={24} sm={12}>
-              <FilmPoster film={film} onError={this.onError} />
-            </Col>
-          ))}
-          {isError}
+          <Online>
+            {spin}
+            {films.map((film) => (
+              <Col key={film.id} xs={24} sm={12}>
+                <FilmPoster film={film} onError={this.onError} />
+              </Col>
+            ))}
+            {isError}
+          </Online>
+          <Offline>
+            <Alert message={error} description="You're offline right now. Check your connection." type="info" />
+          </Offline>
         </Row>
       </section>
     );
--- src/components/loader/loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './loader.css';
-import { Spin } from 'antd';
-
-const Loader = () => {
-  return <Spin spinning fullscreen />;
-};
-
-export default Loader;
